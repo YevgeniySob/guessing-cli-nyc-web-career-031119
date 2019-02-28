80f7532e5332ef3b46565de5634371c69088f5fe
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-28 04:03:55 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,14 +4,15 @@ def run_guessing_game
   puts "Guess a number between 1 and 6."
   input = gets.chomp
   random_num = get_random_num(1, 6)
+  if input == "exit"
+    finito
+  end
   if input.to_i == random_num
     puts "You guessed the correct number!"
   else 
     puts "The computer guessed #{random_num}."
   end
-  if input == "exit"
-    finito
-  end
+  
 end
 
 def get_random_num(from, to)
